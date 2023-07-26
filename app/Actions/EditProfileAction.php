f47eda8741d1b6353aa9b8a5e39e365764881875
lulung <alungsatrio12@gmail.com>
2023-07-26 15:46:43 +0700

    install laravel action and create action for Profile actions

---
+++ app/Actions/EditProfileAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class EditProfileAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
